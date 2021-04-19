4a1d014dcf75f7149866ea13dec8e9dfb57804f0
knoxknot <samuel.nwoye@yahoo.com>
2021-04-18 21:48:15 -0500

    update: added some logging

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
-import os
+import os,logging
 from flask import Flask
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 @app.route("/")
 def hello():
+  logging.debug("saying hello")
   return "Hello Dreamforce"
